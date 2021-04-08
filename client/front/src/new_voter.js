a6ad8c7634851d7159da521c604c7953ae8281cd
Amogh24 <amoghsach@gmail.com>
2021-04-09 01:19:22 +0530

    event handlers

--- client/front/src/new_voter.js
+++ client/front/src/new_voter.js
@@ -17,26 +17,74 @@ const addPerson = () =>
 
 }
 
+//name
+const handleName = (event) =>
+{
+ console.log(event.target.value)
+ setName(event.target.value)
+}
+//age
+const handleAge = (event) =>
+{
+ console.log(event.target.value)
+ setAge(event.target.value)
+}
+//gender
+const handleGender = (event) =>
+{
+ console.log(event.target.value)
+ setGender(event.target.value)
+}
+//mail
+const handleMail = (event) =>
+{
+ console.log(event.target.value)
+ setMail(event.target.value)
+}
+//address
+const handleAddress = (event) =>
+{
+    console.log(event.target.value)
+    setAddress(event.target.value)
+}
+//phone
+const handlePhone = (event) =>
+{
+ console.log(event.target.value)
+ setPhone(event.target.value)
+}
+//date
+const handleDate = (event) =>
+{
+ console.log(event.target.value)
+ setDate(event.target.value)
+}
+//constituency
+const handleConsti = (event) =>
+{
+ console.log(event.target.value)
+ setConsti(event.target.value)
+}
  return(
      <div>
          <h1 className='text-center mt-5'>New Voter registration</h1>
          <form onSubmit ={addPerson}>
              <div>
-                 name: <input value = {newName}
+                 name: <input value = {name}
                  onChange = {handleName}/>
-                 age: <input value = {newAge}
+                 age: <input value = {age}
                  onChange = {handleAge}/>
-                 gender: <input value = {newGender}
+                 gender: <input value = {gender}
                  onChange = {handleGender}/>
-                 mail: <input value = {newMail}
+                 mail: <input value = {mail}
                  onChange = {handleMail}/>
-                 address: <input value = {newAddress}
+                 address: <input value = {address}
                  onChange = {handleAddress}/>
-                 phone: <input value = {newPhone}
+                 phone: <input value = {phone}
                  onChange = {handlePhone}/>
-                 date: <input value = {newDate}
+                 date: <input value = {date}
                  onChange = {handleDate}/>
-                 constituency: <input value = {newConsti}
+                 constituency: <input value = {consti}
                  onChange = {handleConsti}/>
 
              </div>
