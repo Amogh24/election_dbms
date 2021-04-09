82343ac4b8c029c5a7c8ef3e90c1ad413bcf0f50
Amogh24 <amoghsach@gmail.com>
2021-04-10 01:22:48 +0530

    added consti name to officers

--- client/front/src/officer.js
+++ client/front/src/officer.js
@@ -1,17 +1,67 @@
 import React, { useEffect,useState } from 'react';
 
+const func = (x) =>
+{
+    if(x.consti_id === 1)
+    {
+        x.consti_name = 'New Delhi'
+    }
+    else if(x.consti_id === 34)
+    {
+        x.consti_name = 'Chandni Chowk'
+    }
+    else if(x.consti_id === 35)
+    {
+        x.consti_name = 'North East Delhi'
+    }
+    else if(x.consti_id === 36)
+    {
+        x.consti_name = 'East Delhi'
+    }
+    else if(x.consti_id === 37)
+    {
+        x.consti_name = 'North West Delhi'
+    }
+    else if(x.consti_id === 38)
+    {
+        x.consti_name = 'West Delhi'
+    }
+    else 
+    {
+        x.consti_name = 'South Delhi'
+    }
+}
+
 const Officer = () =>
 {
 
+    const [officers,setOfficers] = useState([])
+    const [constis,setconstis] = useState([]) 
     const getOfficers = async()=>
     {
         const response = await fetch('http://localhost:5000/officer')
         const data = await response.json()
         console.log(data)
+        setOfficers(data)
     }
+    
+    const getConsti = async()=>
+    {
+        const res = await fetch('http://localhost:5000/consti')
+        const consti = await res.json()
+        console.log(consti)
+        setconstis(consti)
+    }
+
 useEffect(()=>{
     getOfficers();
 },[])
+useEffect(()=>{
+    getConsti();
+},[])
+
+officers.forEach(func)
+console.log(officers)
 return(
     <div>
          <table className="table">
