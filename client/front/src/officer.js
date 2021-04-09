4fdfb22a390d930f7ede9a8d2ce6967562b0853f
Amogh24 <amoghsach@gmail.com>
2021-04-10 01:30:29 +0530

    created basic officer table

--- client/front/src/officer.js
+++ client/front/src/officer.js
@@ -67,27 +67,24 @@ return(
          <table className="table">
     <thead>
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+        <th>Officer ID</th>
+        <th>Officer Name</th>
+        <th>Constituency</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
+      {/*<tr>
         <td>John</td>
         <td>Doe</td>
         <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
+      </tr>*/}
+     {officers.map(officer => (
+         <tr key={officer.officer_id}>
+             <td>{officer.officer_id}</td>
+             <td>{officer.name}</td>
+             <td>{officer.consti_name}</td>
+         </tr>
+     ))}
     </tbody>
   </table>
     </div>
